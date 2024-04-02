bff564b6ba3505da9145a61b02bc7e0f95383b61
Yahzi Emiliano Martínez García <77820643+yahziem@users.noreply.github.com>
2024-04-02 04:19:29 -0600

    git add.

---
+++ Recursos/js/carta.js
@@ -0,0 +1,43 @@
+const galleryContainer = document.getElementById('galleryContainer');
+
+// Puedes reemplazar estas URL con las de tus propias imágenes
+const imageUrls = [
+    'https://yahzi.000webhostapp.com/mensajes/Carta/1.jpg',
+    'https://yahzi.000webhostapp.com/mensajes/Carta/2.jpg',
+    'https://yahzi.000webhostapp.com/mensajes/Carta/3.jpg',
+    'https://yahzi.000webhostapp.com/mensajes/Carta/4.jpg',
+    'https://yahzi.000webhostapp.com/mensajes/Carta/5.jpg',
+    'https://yahzi.000webhostapp.com/mensajes/Carta/6.jpg',
+
+    
+    // Agrega más URL de imágenes según sea necesario
+];
+
+function createGalleryItem(url) {
+    const item = document.createElement('div');
+    item.classList.add('gallery-item');
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+    const img = document.createElement('img');
+    img.src = url;
+    img.alt = url; // Cambiado para mostrar la URL de la imagen en caso de que el enlace esté roto
+
+    // Comentar las siguientes líneas para usar el tamaño original de las imágenes
+    // img.style.width = '500px';
+    // img.style.height = '500px';
+    // img.style.objectFit = 'cover';
+
+    // Agregar evento de clic para agrandar la imagen
+    imgContainer.addEventListener('click', () => {
+        imgContainer.classList.toggle('enlarged');
+    });
+
+    imgContainer.appendChild(img);
+    item.appendChild(imgContainer);
+    galleryContainer.appendChild(item);
+}
+
+// Crear elementos de la galería para cada URL de imagen
+imageUrls.forEach(url => createGalleryItem(url));
